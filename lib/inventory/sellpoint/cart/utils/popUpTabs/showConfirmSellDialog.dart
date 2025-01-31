d0446657f86b03ebe73e44372821254b0bf0dfab
AntonioRiosCovian <joserioscovian@gmail.com>
2025-01-31 10:07:51 -0600

    sell confirmation dialog styles updated

--- lib/inventory/sellpoint/cart/utils/popUpTabs/showConfirmSellDialog.dart
+++ lib/inventory/sellpoint/cart/utils/popUpTabs/showConfirmSellDialog.dart
@@ -16,132 +16,163 @@ Future<Map<String, dynamic>?> showConfirmSellDialog(BuildContext context) async 
             child: Center(
               child: Container(
                 margin: EdgeInsets.symmetric(
-                    horizontal: MediaQuery.of(context).size.width * 0.04),
+                    horizontal: MediaQuery.of(context).size.width * 0.05),
                 width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height * 0.40,
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(15),
                   color: AppColors.whiteColor,
                 ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Material(
-                      color: Colors.transparent,
-                      child: Text(
-                        'Confirmar venta',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: MediaQuery.of(context).size.width * 0.07,
-                          color: AppColors.primaryColor,
+                padding: EdgeInsets.symmetric(vertical: MediaQuery.of(context).size.height * 0.02, horizontal: MediaQuery.of(context).size.width * 0.05),
+                child: IntrinsicHeight(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Material(
+                        color: Colors.transparent,
+                        child: Text(
+                          'Confirmar venta',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: MediaQuery.of(context).size.width * 0.08,
+                            color: AppColors.primaryColor,
+                          ),
                         ),
                       ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.symmetric(
-                          horizontal: MediaQuery.of(context).size.width * 0.07,
-                          vertical: MediaQuery.of(context).size.height * 0.02),
-                      child: Text(
-                        '¿Seguro que quieres completar esta venta?',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontSize: MediaQuery.of(context).size.width * 0.045,
-                          color: AppColors.blackColor,
+                      Padding(
+                        padding: EdgeInsets.symmetric(
+                            vertical: MediaQuery.of(context).size.height * 0.02),
+                        child: Text(
+                          '¿Seguro que quieres completar esta venta?',
+                          textAlign: TextAlign.start,
+                          style: TextStyle(
+                            fontSize: MediaQuery.of(context).size.width * 0.045,
+                            color: AppColors.blackColor,
+                          ),
                         ),
                       ),
-                    ),
-                    CheckboxListTile(
-                      contentPadding: EdgeInsets.symmetric(
-                          horizontal: MediaQuery.of(context).size.width * 0.07),
-                      title: Text(
-                        'Pago con tarjeta',
-                        style: TextStyle(
-                          fontSize: MediaQuery.of(context).size.width * 0.045,
-                          color: AppColors.blackColor,
-                        ),
-                      ),
-                      value: isCardPayment,
-                      onChanged: (bool? value) {
-                        setState(() {
-                          isCardPayment = value ?? false;
-                        });
-                      },
-                    ),
-                    CheckboxListTile(
-                      contentPadding: EdgeInsets.symmetric(
-                          horizontal: MediaQuery.of(context).size.width * 0.07),
-                      title: Text(
-                        'Imprimir ticket',
-                        style: TextStyle(
-                          fontSize: MediaQuery.of(context).size.width * 0.045,
-                          color: AppColors.blackColor,
-                        ),
-                      ),
-                      value: shouldPrint,
-                      onChanged: (bool? value) {
-                        setState(() {
-                          shouldPrint = value ?? false;
-                        });
-                      },
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        TextButton(
-                          onPressed: () {
-                            Navigator.of(context).pop(null);
-                          },
-                          child: Container(
-                            padding: EdgeInsets.symmetric(
-                                horizontal: MediaQuery.of(context).size.width * 0.03),
-                            decoration: const BoxDecoration(
-                              border: Border(
-                                bottom: BorderSide(
-                                  color: AppColors.redDelete,
-                                  width: 2.5,
-                                ),
-                              ),
+                      Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          border: const Border(
+                            bottom: BorderSide(
+                              color: AppColors.primaryColor,
+                              width: 2.5,
                             ),
-                            child: Text(
-                              'Cancelar',
-                              style: TextStyle(
-                                fontSize: MediaQuery.of(context).size.width * 0.05,
-                                color: AppColors.redDelete,
-                              ),
+                            top: BorderSide(
+                              color: AppColors.primaryColor,
+                              width: 2.5,
+                            ),
+                            left: BorderSide(
+                              color: AppColors.primaryColor,
+                              width: 2.5,
+                            ),
+                            right: BorderSide(
+                              color: AppColors.primaryColor,
+                              width: 2.5,
                             ),
                           ),
                         ),
-                        TextButton(
-                          onPressed: () {
-                            Navigator.of(context).pop({
-                              'isCardPayment': isCardPayment,
-                              'shouldPrint': shouldPrint,
-                            });
-                          },
-                          child: Container(
-                            padding: EdgeInsets.symmetric(
-                                horizontal: MediaQuery.of(context).size.width * 0.03),
-                            decoration: const BoxDecoration(
-                              border: Border(
-                                bottom: BorderSide(
+                        child: Column(
+                          children: [
+                            CheckboxListTile(
+                              contentPadding: EdgeInsets.symmetric(
+                                  horizontal: MediaQuery.of(context).size.width * 0.07),
+                              title: Text(
+                                'Pago con tarjeta',
+                                style: TextStyle(
+                                  fontSize: MediaQuery.of(context).size.width * 0.04,
+                                  color: AppColors.blackColor,
+                                ),
+                              ),
+                              value: isCardPayment,
+                              onChanged: (bool? value) {
+                                setState(() {
+                                  isCardPayment = value ?? false;
+                                });
+                              },
+                            ),
+                            CheckboxListTile(
+                              contentPadding: EdgeInsets.symmetric(
+                                  horizontal: MediaQuery.of(context).size.width * 0.07),
+                              title: Text(
+                                'Imprimir ticket',
+                                style: TextStyle(
+                                  fontSize: MediaQuery.of(context).size.width * 0.04,
+                                  color: AppColors.blackColor,
+                                ),
+                              ),
+                              value: shouldPrint,
+                              onChanged: (bool? value) {
+                                setState(() {
+                                  shouldPrint = value ?? false;
+                                });
+                              },
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: MediaQuery.of(context).size.width * 0.04),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            TextButton(
+                              onPressed: () {
+                                Navigator.of(context).pop(null);
+                              },
+                              child: Container(
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: MediaQuery.of(context).size.width * 0.05,
+                                ),
+                                decoration: const BoxDecoration(
+                                  border: Border(
+                                    bottom: BorderSide(
+                                      color: AppColors.primaryColor,
+                                      width: 2.5,
+                                    ),
+                                  ),
+                                ),
+                                child: Text(
+                                  'Cancelar',
+                                  style: TextStyle(
+                                    fontSize: MediaQuery.of(context).size.width * 0.05,
+                                    color: AppColors.primaryColor,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            TextButton(
+                              onPressed: () {
+                                Navigator.of(context).pop({
+                                  'isCardPayment': isCardPayment,
+                                  'shouldPrint': shouldPrint,
+                                });
+                              },
+                              child: Container(
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: MediaQuery.of(context).size.width * 0.05,
+                                    vertical: MediaQuery.of(context).size.height * 0.01
+                                ),
+                                decoration: BoxDecoration(
                                   color: AppColors.primaryColor,
-                                  width: 2.5,
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                child: Text(
+                                  'Confirmar',
+                                  style: TextStyle(
+                                    fontSize: MediaQuery.of(context).size.width * 0.05,
+                                    color: AppColors.whiteColor,
+                                  ),
                                 ),
                               ),
                             ),
-                            child: Text(
-                              'Confirmar',
-                              style: TextStyle(
-                                fontSize: MediaQuery.of(context).size.width * 0.05,
-                                color: AppColors.primaryColor,
-                              ),
-                            ),
-                          ),
+                          ],
                         ),
-                      ],
-                    ),
-                  ],
-                ),
+                      )
+                    ],
+                  ),
+                )
               ),
             ),
           );

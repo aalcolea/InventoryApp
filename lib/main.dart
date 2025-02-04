62a9bbd8739802bc26a7e38566f96694ebf4310f
aalcolea <alanalcolea@gmail.com>
2025-02-04 01:27:26 -0600

    fixing endpoints auth service and adding simulator ios performace

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:inventory_app/inventory/deviceManager.dart';
@@ -10,10 +13,11 @@ import 'helpers/views/login.dart';
 import 'inventory/sellpoint/cart/services/cartService.dart';
 
 final RouteObserver<PageRoute> routeObserver = RouteObserver<PageRoute>();
-void main() async {
 
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+
   FirebaseMessaging messaging = FirebaseMessaging.instance;
   NotificationSettings settings = await messaging.requestPermission(
     alert: true,
@@ -21,21 +25,36 @@ void main() async {
     sound: true,
   );
   print('Permiso de notificación aceptado: ${settings.authorizationStatus}');
-  try {
-    String? fcmToken = await messaging.getToken();
-    print('FCM Token: $fcmToken');
-  } catch (e) {
-    print('Error al obtener el token FCM: $e');
+
+  bool isSimulator = await isRunningOnSimulator();
+  print('es un simulador de ios? $isSimulator');
+
+  if (!isSimulator) {
+    try {
+      String? fcmToken = await messaging.getToken();
+      print('FCM Token: $fcmToken');
+    } catch (e) {
+      print('Error al obtener el token FCM: $e');
+    }
+  } else {
+    print('Ejecutando en un simulador de iOS');
   }
+
   FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-    print('Mensaje recibido en primer plano (app abierta): ${message.notification?.title}');
+    print('Mensaje recibido en primer plano: ${message.notification?.title}');
   });
-  runApp(MultiProvider(
-    providers: [ChangeNotifierProvider(create: (_) => CartProvider())],
-    child: const MyApp(),),
+
+  runApp(
+    MultiProvider(
+      providers: [ChangeNotifierProvider(create: (_) => CartProvider())],
+      child: const MyApp(),
+    ),
   );
 }
-
+Future<bool> isRunningOnSimulator() async {
+  if (!Platform.isIOS) return false;
+  return kDebugMode && !kIsWeb;
+}
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
